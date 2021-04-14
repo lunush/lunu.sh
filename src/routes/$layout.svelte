d18ff291ba2ef1f256d1834a19c9e4015788ca74
Lunush <76045303+lunush@users.noreply.github.com>
2021-04-14 17:32:44 +0300

    Minor changes

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,7 +1,7 @@
-<nav class="h-16 px-6 flex items-center w-full">
+<nav class="h-16 px-6 flex items-center w-full shadow">
 	<a class="mr-auto text-xl" href=".">Home</a>
 	<a class="mr-4 text-xl" href="portfolio">Portfolio</a>
-	<a class="text-xl" href="blog">Blog</a>
+	<a class="text-xl" href="contact">Contact</a>
 </nav>
 
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,3 @@
-<main>
+<main class="shadow">
 	<h1 class="text-3xl">Index</h1>
 </main>
