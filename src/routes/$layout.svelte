2f4f02cfae35a587cd1f4d37b60c822c6ff0ff77
Lunush <76045303+lunush@users.noreply.github.com>
2021-04-14 19:08:50 +0300

    Add ugly form

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,7 +1,7 @@
-<nav class= p-6 flex w-full">
-	<a class="mr-auto text-xl" href=".">Home</a>
-	<a class="mr-4 text-xl" href="portfolio">Portfolio</a>
-	<a class="text-xl" href="contact">Contact</a>
+<nav class="p-6 flex w-full">
+	<a class="mr-auto text-xl" href="/">Home</a>
+	<a class="mr-4 text-xl" href="/portfolio">Portfolio</a>
+	<a class="text-xl" href="/contact">Contact</a>
 </nav>
 
 <main class="h-full bg-gradient-to-b from-[#fafafa] to-white">
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,25 +1,25 @@
-<div class="m-auto w-full h-full grid place-items-center border">
+<div class="m-auto w-full grid place-items-center">
 	<form
 		class="flex flex-col"
 		action="https://formsubmit.co/f6ad74133bb13c93dbc668bd14c9d6a5"
 		method="POST"
 	>
 		<input
-			class="border mx-auto p-4 rounded-lg my-4 text-md"
+			class="border mx-auto p-2 rounded-lg my-2 text-md"
 			type="text"
 			name="name"
 			placeholder="Your Name"
 			required
 		/>
 		<input
-			class="border mx-auto p-4 rounded-lg my-4 text-md"
+			class="border mx-auto p-2 rounded-lg my-2 text-md"
 			type="email"
 			name="email"
 			placeholder="Your Email"
 			required
 		/>
 		<textarea
-			class="border mx-auto p-4 rounded-lg my-4 text-sm"
+			class="border mx-auto p-2 rounded-lg my-2 text-sm"
 			type="text"
 			name="message"
 			placeholder="Message"
@@ -28,7 +28,7 @@
 		<input type="text" name="_honey" style="display:none" />
 		<input type="hidden" name="_captcha" value="false" />
 		<button
-			class="border mx-auto p-4 rounded-lg my-4 text-lg bg-green-400
+			class="border mx-auto p-4 rounded-lg my-2 text-lg bg-green-400
   text-white"
 			type="submit">Send</button
 		>
