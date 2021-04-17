3ca981612fe385607236c16033851d4d51103f08
Lunush <76045303+lunush@users.noreply.github.com>
2021-04-17 08:17:58 +0300

    Add index page

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,9 +1,11 @@
-<nav class="p-6 flex w-full">
-	<a class="mr-auto text-xl" href="/">Home</a>
-	<a class="mr-4 text-xl" href="/portfolio">Portfolio</a>
-	<a class="text-xl" href="/contact">Contact</a>
-</nav>
+<div class="h-full flex flex-col">
+	<nav class="p-6 flex w-full">
+		<a class="mr-auto text-xl" href="/">Home</a>
+		<a class="mr-4 text-xl" href="/portfolio">Portfolio</a>
+		<a class="text-xl" href="/contact">Contact</a>
+	</nav>
 
-<main class="h-full bg-gradient-to-b from-[#fafafa] to-white">
-	<slot />
-</main>
+	<main class="relative h-full top-0 bottom-0 bg-gradient-to-b from-gray-50 to-white">
+		<slot />
+	</main>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,48 @@
-<main class="shadow">
-	<h1 class="text-3xl">Index</h1>
+<script>
+	import Avatar from '../assets/images/avatar.png';
+</script>
+
+<main class="grid place-items-center h-full w-full">
+	<div class="flex flex-col sm:flex-row align-middle items-center">
+		<img src={Avatar} alt="Avatar" class="h-60 mb-6 sm:mb-0 sm:mr-6" />
+		<div class="flex flex-col items-center sm:items-start">
+			<h1 class="text-3xl sm:text-4xl sm:mb-4">Hi there! <span class="wave">👋</span></h1>
+			<h2 class="text-xl sm:text-2xl">I am a self-taught web developer</h2>
+		</div>
+	</div>
 </main>
+
+<style>
+	.wave {
+		animation: wave-animation 2.5s infinite;
+		transform-origin: 70% 70%;
+		display: inline-block;
+	}
+
+	@keyframes wave-animation {
+		0% {
+			transform: rotate(0deg);
+		}
+		10% {
+			transform: rotate(14deg);
+		}
+		20% {
+			transform: rotate(-8deg);
+		}
+		30% {
+			transform: rotate(14deg);
+		}
+		40% {
+			transform: rotate(-4deg);
+		}
+		50% {
+			transform: rotate(10deg);
+		}
+		60% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+</style>
